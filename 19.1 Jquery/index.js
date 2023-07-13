d85e0a0d4794c22edf56c22e3ac874496a4083c9
Aparajit1610 <aparajit1610@gmail.com>
2023-07-13 22:04:15 +0530

    final commit

---
+++ 19.1 Jquery/index.js
@@ -0,0 +1,108 @@
+                   // Another way to write this, jQuery("h1").css("color", "red");
+//$("h1").css("color", "red"); 
+
+                                // Selecting elements with jquery
+//$("h1");
+
+                                // Manupulating styles with jquery
+//$("h1").css("color", "green");
+//console.log($("h1").css("color"));
+//console.log($("h1").css("font-size"));
+//$("h1").addClass("big-title");
+//$("h1").removeClass("big-title");
+//$("h1").addClass("big-title margin-50");
+//$("h1").hasClass("margin-50");
+
+                                // Manupulating texts with jquery
+//$("h1").text("Bye");
+//$("button").text("Don't Click Me");
+//$("button").html("<em>Hey</em>");
+//$("button").text("<em>Hey</em>");
+
+                             // Manupulating attributes with jquery
+//console.log($("img").attr("src"));
+//$("a").attr("href", "https://www.yahoo.com");
+//$("h1").attr("class");
+
+                              // Adding event listners with jquery
+//$("h1").click(function(){
+    //$("h1").css("color", "purple"); 
+//});
+//$("button").click(function(){
+    //$("h1").css("color", "purple"); 
+//});
+//$("input").keypress(function(event){
+    //console.log(event.key); 
+//});
+//$("document").keypress(function(event){
+    //console.log(event.key); 
+//});
+//$("document").keypress(function(event){
+    //$("h1").text(event.key); 
+//});
+//$("h1").on("mouseover", function(){
+    //$("h1").css("color", "purple"); 
+//});
+
+                        // Adding and removing elements with jquery
+//$("h1").before("<button>New</button>");
+//$("h1").after("<button>New</button>");
+//$("h1").prepend("<button>New</button>");
+//$("h1").append("<button>New</button>");
+//$("button").remove();
+
+                           // Website animations with jquery
+//$("h1").on("click", function(){
+    //$("h1").hide()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").show()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").toggle()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").fadeOut()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").fadeIn()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").fadeToggle()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").slideUp()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").slideDown()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").slideToggle()); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").animate({opacity: 0.5})); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").animate({margin: 20})); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").animate({margin: "20%"})); 
+//});
+//$("h1").on("click", function(){
+    //$("h1").slideUp.slideDown.animate({opacity: 0.5})); 
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
